f90243c73e9a7e0a9208526e7fdd8a82db30d654
zhanghaifeng2019 <zhang@rentracks.co.jp>
2022-01-09 16:32:59 +0900

    test commit

--- _test_/demo.test.js
+++ _test_/demo.test.js
@@ -14,5 +14,5 @@ test('10 plus 20 should be 30', () => {
 
 test('10 plus 20 should not be 40', () => {
     const result = sum(10, 20)
-    expect(result).not.toBe(40)
+    expect(result).not.toBe(100)
 })

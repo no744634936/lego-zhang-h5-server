393b5ee314fbcbb87a9efeffbb76d44a62567357
zhanghaifeng2019 <zhang@rentracks.co.jp>
2022-01-21 14:40:49 +0900

    vue3_SSR

--- readme.md
+++ readme.md
@@ -1,11 +1,47 @@
-查看explaination/create_and_publish_work.png 
+1，npm install vue@next
 
-可以知道editor-server项目中作品是如何储存，如何发布的
+2，npm i @vue/server-renderer
+
+3，使用vue3的服务器端渲染（SSR）
+
+作为练习，查看explaination/vue3_SSR_practice文件夹里的文件
+
+4，npm i lego-components
+
+
+5，查看routes/woks.js 文件中的两个路由
+
+这节的主要内容是将已发布的作品取出来，
+
+做成一个html字符串
+
+然后将这个html字符串交给 views/works.js 文件，
+
+渲染成完整的网页
+
+
+----------------------------------------------------
+测试流程
+
+首先使用editor_backend_server里的 api
+
+localhost:3000/api/works/create
+
+然后使用
+
+explaination/published_work_example.json
+
+文件里的数据创建一个作品
+
+然后使用api
+
+localhost:3000/api/works/publish/:id
+
+发布该作品得到一个链接:
+
+http://localhost:3001/p/143-9b48
+
+浏览器输入该链接就可以查看结果了
 
 
 
-routes/work.js 中有两个路由
-
-获取已发布的作品: http://localhost:3001/p/137-acb3
-
-预览作品: http://localhost:3001/p/preview/137-acb3
--- src/routes/works.js
+++ src/routes/works.js
@@ -8,10 +8,36 @@
 const router = require('koa-router')()
 const { splitIdAndUuid } = require('../utils/util')
 const { findPublishWork, findPreviewWork } = require('../controller/works')
-
+const { propsToStyle, getLegoComponentsHtml } = require('../utils/render')
 // 路由前缀
 router.prefix('/p')
 
+/**
+ * 渲染页面
+ * @param {object} ctx ctx
+ * @param {object} work work 已发布的作品信息
+ * @param {string} pageType 'publish' / 'template'
+ */
+async function renderPage(ctx, work, pageType) {
+    const { title, desc = '', content = {} } = work
+    const { props = {}, components = [], setting = {} } = content
+
+    const bodyStyle = propsToStyle(props)
+    const componentsHtml = await getLegoComponentsHtml(components)
+
+    // 渲染页面
+    await ctx.render('work', {
+        title,
+        desc,
+        bodyStyle,
+        content: componentsHtml,
+        pageType,
+    })
+}
+
+// 从数据库中找到已发布作品的json，然后将之做成一个html字符串，
+// 然后将这个html字符串交给 views/works.js 文件，
+// 渲染成完整的网页
 router.get('/:idAndUuid', async ctx => {
     const { idAndUuid } = ctx.params // 链接localhost:3001/p/137-acb3 其中137-acb3就是idAndUuid'
     const { id, uuid } = splitIdAndUuid(idAndUuid) // { id: '10', uuid: 'abc' }
@@ -23,11 +49,19 @@ router.get('/:idAndUuid', async ctx => {
         return
     }
 
-    // 渲染页面内容
-    await ctx.render('work', {
-        // 后面要承载 Vue3 SSR 的结果 - html 标签
-        content: `<div style="color: blue;">${JSON.stringify(work)}</div>`,
-    })
+    // // 渲染页面内容
+    // await ctx.render('work', {
+    //     // 后面要承载 Vue3 SSR 的结果 - html 标签
+    //     content: `<div style="color: blue;">${JSON.stringify(work)}</div>`,
+    // })
+
+    if (work.isTemplate) {
+        // 模板
+        await renderPage(ctx, work, 'template')
+    } else {
+        // 正常页面
+        await renderPage(ctx, work, 'publish')
+    }
 })
 
 // 任何作品都可以预览，不限于已发布作品
@@ -43,11 +77,13 @@ router.get('/preview/:idAndUuid', async ctx => {
         return
     }
 
-    // 渲染页面内容
-    await ctx.render('work', {
-        content: `<div style="color: blue;">${JSON.stringify(work)}</div>`,
-        pageType: 'preview',
-    })
+    // // 渲染页面内容
+    // await ctx.render('work', {
+    //     content: `<div style="color: blue;">${JSON.stringify(work)}</div>`,
+    //     pageType: 'preview',
+    // })
+    // 渲染页面
+    await renderPage(ctx, work, 'preview')
 })
 
 module.exports = router
